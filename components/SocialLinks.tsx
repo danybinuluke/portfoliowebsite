c63b64e24aa3f6c2c6ba9c42bf85b6bf84cdacab
Dany Binu Luke <danybinu2005@gmail.com>
2026-01-26 12:34:45 +0530

    Updated Leetcode and Codeforces Logo

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,9 +1,9 @@
-import { Braces, Code, CodeXml, Github, Instagram, Linkedin, Mails } from 'lucide-react';
+import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 
-const socialData=[
+const socialData = [
   {
     title: "Instagram",
     icon: <Instagram className='w-5 h-5' />,
@@ -11,7 +11,7 @@ const socialData=[
   },
   {
     title: "GitHub",
-    icon: <Github className='w-5 h-5'/>,
+    icon: <Github className='w-5 h-5' />,
     link: "https://www.github.com/danybinuluke",
   },
   {
@@ -21,39 +21,64 @@ const socialData=[
   },
   {
     title: "LeetCode",
-    icon: <Code className='w-5 h-5' />,
+    // This div uses bg-current to match the text color of the parent Link
+    icon: (
+      <div
+        className="w-5 h-5 bg-current"
+        style={{
+          mask: 'url(https://simpleicons.org/icons/leetcode.svg) no-repeat center',
+          WebkitMask: 'url(https://simpleicons.org/icons/leetcode.svg) no-repeat center',
+          maskSize: 'contain',
+          WebkitMaskSize: 'contain'
+        }}
+      />
+    ),
     link: "https://leetcode.com/u/ExTerminator969",
   },
   {
     title: "Codeforces",
-    icon: <CodeXml className='w-5 h-5' />,
+    icon: (
+      <div
+        className="w-5 h-5 bg-current"
+        style={{
+          mask: 'url(https://simpleicons.org/icons/codeforces.svg) no-repeat center',
+          WebkitMask: 'url(https://simpleicons.org/icons/codeforces.svg) no-repeat center',
+          maskSize: 'contain',
+          WebkitMaskSize: 'contain'
+        }}
+      />
+    ),
     link: "https://codeforces.com/profile/Exterminator969",
   },
   {
     title: "Gmail",
-    icon: <Mails className='w-5 h-5' />,
+    icon: <Mail className='w-5 h-5' />,
     link: "mailto:danybinu2005@gmail.com",
   },
 ];
 
-
 const SocialLinks = () => {
   return (
     <TooltipProvider>
-        <div className='flex items-center gap-3'>
-        {socialData?.map((item)=>(
-            <Tooltip key={item?.title}>
-                <TooltipTrigger asChild>
-                <Link href={item?.link} className="text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:text-lightSky hoverEffect">
-            {item?.icon}
-            </Link>
+      <div className='flex items-center gap-3'>
+        {socialData?.map((item) => (
+          <Tooltip key={item?.title}>
+            <TooltipTrigger asChild>
+              <Link 
+                href={item?.link} 
+                className="text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:text-lightSky hoverEffect flex items-center justify-center"
+              >
+                {item?.icon}
+              </Link>
             </TooltipTrigger>
-            <TooltipContent className='text-xs uppercase font-medium rounded-[6px]'>{item?.title}</TooltipContent>
-            </Tooltip>
+            <TooltipContent className='text-xs uppercase font-medium rounded-[6px]'>
+              {item?.title}
+            </TooltipContent>
+          </Tooltip>
         ))}
-    </div>
+      </div>
     </TooltipProvider>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
