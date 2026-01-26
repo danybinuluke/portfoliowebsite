93dfceb3814dcb00d98863054391ea0855e8cc54
Dany Binu Luke <danybinu2005@gmail.com>
2026-01-26 12:45:46 +0530

    Updated Icons

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,7 +1,21 @@
 import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
+import { siLeetcode, siCodeforces } from 'simple-icons';
 import Link from 'next/link';
 import React from 'react';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+
+// Fixed Helper: Correctly accesses the 'path' property from simple-icons
+const SiIcon = ({ icon }) => (
+  <svg
+    role="img"
+    viewBox="0 0 24 24"
+    className="w-5 h-5 fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {/* Simple Icons provides the path as a raw string */}
+    <path d={icon.path} />
+  </svg>
+);
 
 const socialData = [
   {
@@ -21,33 +35,12 @@ const socialData = [
   },
   {
     title: "LeetCode",
-    // This div uses bg-current to match the text color of the parent Link
-    icon: (
-      <div
-        className="w-5 h-5 bg-current"
-        style={{
-          mask: 'url(https://simpleicons.org/icons/leetcode.svg) no-repeat center',
-          WebkitMask: 'url(https://simpleicons.org/icons/leetcode.svg) no-repeat center',
-          maskSize: 'contain',
-          WebkitMaskSize: 'contain'
-        }}
-      />
-    ),
+    icon: <SiIcon icon={siLeetcode} />,
     link: "https://leetcode.com/u/ExTerminator969",
   },
   {
     title: "Codeforces",
-    icon: (
-      <div
-        className="w-5 h-5 bg-current"
-        style={{
-          mask: 'url(https://simpleicons.org/icons/codeforces.svg) no-repeat center',
-          WebkitMask: 'url(https://simpleicons.org/icons/codeforces.svg) no-repeat center',
-          maskSize: 'contain',
-          WebkitMaskSize: 'contain'
-        }}
-      />
-    ),
+    icon: <SiIcon icon={siCodeforces} />,
     link: "https://codeforces.com/profile/Exterminator969",
   },
   {
@@ -66,7 +59,7 @@ const SocialLinks = () => {
             <TooltipTrigger asChild>
               <Link 
                 href={item?.link} 
-                className="text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:text-lightSky hoverEffect flex items-center justify-center"
+                className="text-lightSky/80 border border-lightSky/30 p-2.5 rounded-full hover:bg-lightSky/10 hover:text-lightSky hoverEffect flex items-center justify-center transition-all duration-300"
               >
                 {item?.icon}
               </Link>
